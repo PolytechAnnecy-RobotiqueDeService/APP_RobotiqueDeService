941e20f3ec68899b346ecb039ba1d3b3b630b22b
camille-jenny <91551577+camille-jenny@users.noreply.github.com>
2023-02-09 12:07:41 +0100

    New version déplacement 09/02

---
+++ 1_Choregraphe/13_Deplacement/Guide_Pepper/translations/translation_en_US.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>fini</source>
+            <comment>Text</comment>
+            <translation type="obsolete">fini</translation>
+        </message>
+        <message>
+            <source>j'ai terminé</source>
+            <comment>Text</comment>
+            <translation type="obsolete">j'ai terminé</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai commencé</source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai commencé</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai terminé</source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai terminé</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/main_timeline/main_behavior/init/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Okay</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Okay</translation>
+        </message>
+        <message>
+            <source>Okay deux</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Okay deux</translation>
+        </message>
+        <message>
+            <source>j'ai commencé</source>
+            <comment>Text</comment>
+            <translation type="obsolete">j'ai commencé</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/main_timeline/main_behavior/init/Say (1)</name>
+        <message>
+            <source>go</source>
+            <comment>Text</comment>
+            <translation type="obsolete">go</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/main_timeline/main_behavior/init/Say (2)</name>
+        <message>
+            <source>Okay un</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Okay un</translation>
+        </message>
+    </context>
+</TS>
