ea897167d08bce9302d0fa4d6b6a8e85d1e4a856
camille-jenny <91551577+camille-jenny@users.noreply.github.com>
2023-03-31 08:14:30 +0200

    Add files via upload

---
+++ 1_Choregraphe/Programme_final/translations/translation_en_US.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/test_tout/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>je suis arrivé</source>
+            <comment>Text</comment>
+            <translation type="unfinished">je suis arrivé</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>j'ai pas pu avancer</source>
+            <comment>Text</comment>
+            <translation type="unfinished">j'ai pas pu avancer</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/test_tout/Say (1)</name>
+        <message>
+            <source>j'ai pas pu avancer</source>
+            <comment>Text</comment>
+            <translation type="obsolete">j'ai pas pu avancer</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/virage/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>je suis arrivé</source>
+            <comment>Text</comment>
+            <translation type="unfinished">je suis arrivé</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/securite/surchauffe_et_batterie/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ma batterie est faible, je risque de m'éteindre. Laissez moi ici et alertez quelqu'un de ma situation.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ma batterie est faible, je risque de m'éteindre. Laissez moi ici et alertez quelqu'un de ma situation.</translation>
+        </message>
+    </context>
+</TS>
