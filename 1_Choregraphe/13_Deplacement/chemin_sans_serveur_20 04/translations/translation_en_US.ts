5f25670576ebc7a8dd4fe80efd4f4f22132b6744
camille-jenny <91551577+camille-jenny@users.noreply.github.com>
2023-04-26 11:22:14 +0200

    Add files via upload

---
+++ 1_Choregraphe/13_Deplacement/chemin_sans_serveur_20 04/translations/translation_en_US.ts
@@ -0,0 +1,136 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/suivi_distance/Say</name>
+        <message>
+            <source>Je suis dans suivi distance</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Je suis dans suivi distance</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/suivi_intersection/Say</name>
+        <message>
+            <source>Je suis dans suivi intersection</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Je suis dans suivi intersection</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/test_tout/Say</name>
+        <message>
+            <source>je suis arrivé</source>
+            <comment>Text</comment>
+            <translation type="obsolete">je suis arrivé</translation>
+        </message>
+        <message>
+            <source>j'ai pas pu avancer</source>
+            <comment>Text</comment>
+            <translation type="obsolete">j'ai pas pu avancer</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/test_tout/Say (1)</name>
+        <message>
+            <source>j'ai pas pu avancer</source>
+            <comment>Text</comment>
+            <translation type="obsolete">j'ai pas pu avancer</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/trajectoire/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Je suis dans trajectoire</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Je suis dans trajectoire</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/deplacement/virage/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>je suis arrivé</source>
+            <comment>Text</comment>
+            <translation type="obsolete">je suis arrivé</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai effectué mon virage</source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai effectué mon virage</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>j'ai pas pu avancer</source>
+            <comment>Text</comment>
+            <translation type="unfinished">j'ai pas pu avancer</translation>
+        </message>
+        <message>
+            <source>Je suis dans virage</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Je suis dans virage</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Deplacement/securite/surchauffe_et_batterie/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ma batterie est faible, je risque de m'éteindre. Laissez moi ici et alertez quelqu'un de ma situation.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ma batterie est faible, je risque de m'éteindre. Laissez moi ici et alertez quelqu'un de ma situation.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/all/behavior_layer1/keyframe1/Deplacement/deplacement/virage/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai effectué mon virage</source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai effectué mon virage</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>j'ai pas pu avancer</source>
+            <comment>Text</comment>
+            <translation type="unfinished">j'ai pas pu avancer</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/all/behavior_layer1/keyframe1/Deplacement/securite/surchauffe_et_batterie/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ma batterie est faible, je risque de m'éteindre. Laissez moi ici et alertez quelqu'un de ma situation.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ma batterie est faible, je risque de m'éteindre. Laissez moi ici et alertez quelqu'un de ma situation.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/all/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>J'ai fini mon programe</source>
+            <comment>Text</comment>
+            <translation type="unfinished">J'ai fini mon programe</translation>
+        </message>
+    </context>
+</TS>
